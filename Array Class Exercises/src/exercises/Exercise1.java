068592afd6c3eb08e0de0df5ab90476d069a962d
aleleotta <aleleotta@106-02.info2>
2023-01-11 13:41:57 +0100

    Exercise 1 finished.

--- Array Class Exercises/src/exercises/Exercise1.java
+++ Array Class Exercises/src/exercises/Exercise1.java
@@ -3,13 +3,16 @@ import java.util.*;
 
 public class Exercise1 {
 	public static void main(String[] args) {
-		int n, m;
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Introduce the amount of positions in the array: ");
-		n = sc.nextInt();
-		System.out.print("Introduce a number to fill in every array position: ");
-		m = sc.nextInt();
-		sc.close();
+		int n, m; //n = amount of positions. m = number to fill in array.
+		Scanner sc = new Scanner(System.in); //Scanner declared.
+		System.out.print("Introduce the amount of positions in the array: "); //Program asks for input.
+		n = sc.nextInt(); //User input
+		System.out.print("Introduce a number to fill in every array position: "); //Program asks for input.
+		m = sc.nextInt(); //User input
+		int table[] = new int[n]; //Declaration of array and assignment of n.
+		Arrays.fill(table, m); //Assignment of m to all array positions.
+		System.out.println(Arrays.toString(table)); //Printing array with toString.
+		sc.close(); //Scanner closed.
 	}
 }
 /*Crea un programa que pida al usuario dos valores N y M y
