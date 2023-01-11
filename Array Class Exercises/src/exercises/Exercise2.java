6e6f970d1924062972be87e5e46d2e7c9507e14b
aleleotta <aleleotta@106-02.info2>
2023-01-11 14:32:47 +0100

    .

--- Array Class Exercises/src/exercises/Exercise2.java
+++ Array Class Exercises/src/exercises/Exercise2.java
@@ -3,11 +3,16 @@ import java.util.*;
 
 public class Exercise2 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.close();
+		int start = 0;
+		int table[] = new int[55];
+		for(int i = 1; i <= 10; i++) {
+			Arrays.fill(table, start, start+i, i);
+			start = start + i;
+		}
+		System.out.println("Array: " + Arrays.toString(table));
 	}
 }
 /*Crea un programa que cree un array de enteros e
  *introduzca la siguiente secuencia de valores: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, etc...
  *hasta introducir 10 diez veces el 10, y luego la muestre por pantalla.
- *En esta ocasión has de utilizar Arrays.fill().*/
+ *En esta ocasión hay que utilizar Arrays.fill().*/
