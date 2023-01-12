0e232d20e33ee84e667767196156cea0cbec3525
aleleotta <aleleotta@106-28.info2>
2023-01-12 14:15:54 +0100

    .

--- Array Class Exercises/src/exercises/Exercise3.java
+++ Array Class Exercises/src/exercises/Exercise3.java
@@ -3,7 +3,24 @@ import java.util.*;
 
 public class Exercise3 {
 	public static void main(String[] args) {
+		int table1[] = new int[10];
+		int table2[] = new int[10];
 		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < table1.length ; i++) {
+			System.out.print("Introduce a number: ");
+			table1[i] = sc.nextInt();
+			System.out.println("\t\t\t\t\t\t\t\t\t\t\t Array 1: " + Arrays.toString(table1));
+		}
+		for(int i = 0; i < table2.length ; i++) {
+			System.out.print("Introduce a number: ");
+			table2[i] = sc.nextInt();
+			System.out.println("\t\t\t\t\t\t\t\t\t\t\t Array 2: " + Arrays.toString(table2));
+		}
+		if (Arrays.equals(table1, table2)) {
+			System.out.println("Both arrays are equal.");
+		} else {
+			System.out.println("The two arrays are different.");
+		}
 		sc.close();
 	}
 }
