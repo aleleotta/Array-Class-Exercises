63d9b8cf443542bf2098024edf0fd3bd4f0b74dc
aleleotta <alessandro.leotta@iesnervion.es>
2023-01-17 12:47:02 +0100

    .

--- Array Class Exercises/src/exercises/Exercise7.java
+++ Array Class Exercises/src/exercises/Exercise7.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class Exercise7 {
 	public static void main(String[] args) {
-		int table1[] = new int[400];
+		int table1[] = new int[6];
 		int table2[] = new int[6];
 		Scanner sc = new Scanner(System.in);
 		for(int i = 0; i < table1.length ; i++) {
 			table1[i] = (int) (Math.random() * 50) + 1;
 		}
 		System.out.println(Arrays.toString(table1));
+		System.out.println(Arrays.toString(table2));
 		sc.close();
 	}
 }
